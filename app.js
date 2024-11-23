a471907d7d377ba50f459ba4e69f72d84e2e39c6
Stuti <stutirai2060@gmail.com>
2024-11-23 16:22:02 +0000

    MongoDB connection fixed

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const morgan = require("morgan");
-const cors = require ("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors()); 
 
-const mongoURI = "mongodb+srv://Stutirai:stutirai@webstorecluster.scncs.mongodb.net/";
+const mongoURI = "mongodb+srv://Stutirai:stutirai@webstorecluster.scncs.mongodb.net/Webstore?retryWrites=true&w=majority";
 let db;
 
-
 // Connect to MongoDB Atlas
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+MongoClient.connect(mongoURI)
   .then(client => {
     console.log("Connected to MongoDB Atlas!");
     db = client.db("Webstore"); // Specify the database you want to use
+
+    // Start the server once connected to MongoDB
+    app.listen(3000, function () {
+      console.log("App started on port 3000");
+    });
   })
   .catch(err => {
     console.error("Error connecting to MongoDB Atlas", err);
+    process.exit(1); // Exit the process if MongoDB connection fails
   });
 
 // Middleware to log the request method and URL
@@ -27,7 +32,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Start server on port 3000
-app.listen(3000, function () {
-  console.log("App started on port 3000");
+// Optional: Error handling middleware (catch all errors)
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
 });
