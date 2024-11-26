6182c8f45af97a917c7f7c1258fd511ebf22c925
Stuti <stutirai2060@gmail.com>
2024-11-26 21:42:01 +0000

    Updated Routes

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const morgan = require("morgan");
+const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const propertiesReader = require("properties-reader");
+const morgan = require("morgan");
 
 const app = express();
+
+// Middleware for parsing JSON
 app.use(express.json());
+// Enable CORS for all origins
 app.use(cors());
+// Enable HTTP request logging
+app.use(morgan("short"));
 
 // Load the properties from the db.properties file
 let propertiesPath = path.resolve(__dirname, "config/db.properties");
 let properties = propertiesReader(propertiesPath);
 
 // Retrieve the properties
-let dbPrefix = properties.get("db.prefix");
+let dbPprefix = properties.get("db.prefix");
 let dbUsername = encodeURIComponent(properties.get("db.user"));
 let dbPwd = encodeURIComponent(properties.get("db.pwd"));
 let dbName = properties.get("db.dbName");
@@ -22,82 +27,103 @@ let dbUrl = properties.get("db.dbUrl");
 let dbParams = properties.get("db.params");
 
 // Construct the MongoDB URI
-const uri = dbPrefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
 
 // Create MongoDB client instance
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
-let db;
+let db =  client.db(dbName);
 
-// Middleware to handle collection name
+// Connect to MongoDB Atlas
+client.connect()
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+    
+    // Start the server only after successful connection to MongoDB
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch(err => {
+    console.error("Error connecting to MongoDB Atlas", err);
+    process.exit(1); // Exit if unable to connect to MongoDB
+  });
+
+// Middleware to handle collection name dynamically
 app.param("collectionName", function (req, res, next, collectionName) {
   req.collection = db.collection(collectionName);
   next();
 });
 
-// Middleware to validate request body
-const validatePayload = (req, res, next) => {
-  if (!req.body || Object.keys(req.body).length === 0) {
-    return res.status(400).send({ msg: "Invalid payload" });
-  }
-  next();
-};
-
 // Routes
+
+// GET all documents in the specified collection
 app.get("/collections/:collectionName", function (req, res, next) {
+  console.log("Fetching data from collection:", req.params.collectionName);
   req.collection.find({}).toArray(function (err, results) {
-    if (err) return next(err);
+    if (err) {
+      console.error("Error fetching data:", err);
+      return next(err);
+    }
+    console.log("Results:", results);
     res.send(results);
   });
 });
 
-app.get("/collections/:collectionName/:id", function (req, res, next) {
-  req.collection.findOne({ _id: new ObjectId(req.params.id) }, function (err, results) {
-    if (err) return next(err);
-    res.send(results);
+
+app.get('/collections/:collectionName/:id'
+  , function(req, res, next) {
+   req.collection.findOne({ _id: new ObjectId(req.params.id) }, function(err, results) {
+   if (err) {
+   return next(err);
+   }
+   res.send(results);
+   });
+  });
+
+
+// POST a new document to the specified collection
+app.post("/collections/:collectionName", function (req, res, next) {
+  console.log("Inserting new document into collection:", req.params.collectionName);
+  req.collection.insertOne(req.body, function (err, result) {
+    if (err) {
+      console.error("Error inserting document:", err);
+      return next(err);
+    }
+    res.send(result);
   });
 });
 
-app.post("/collections/:collectionName", validatePayload, function (req, res, next) {
-  req.collection.insertOne(req.body, function (err, results) {
-    if (err) return next(err);
-    res.send(results);
-  });
-});
-
-app.put("/collections/:collectionName/:id", validatePayload, function (req, res, next) {
+// PUT (update) an existing document by ID in the specified collection
+app.put("/collections/:collectionName/:id", function (req, res, next) {
+  console.log("Updating document with ID:", req.params.id);
   req.collection.updateOne(
     { _id: new ObjectId(req.params.id) },
     { $set: req.body },
     { safe: true, multi: false },
     function (err, result) {
-      if (err) return next(err);
+      if (err) {
+        console.error("Error updating document:", err);
+        return next(err);
+      }
       res.send(result.matchedCount === 1 ? { msg: "success" } : { msg: "error" });
     }
   );
 });
 
+// DELETE a document by ID from the specified collection
 app.delete("/collections/:collectionName/:id", function (req, res, next) {
+  console.log("Deleting document with ID:", req.params.id);
   req.collection.deleteOne({ _id: new ObjectId(req.params.id) }, function (err, result) {
-    if (err) return next(err);
+    if (err) {
+      console.error("Error deleting document:", err);
+      return next(err);
+    }
     res.send(result.deletedCount === 1 ? { msg: "success" } : { msg: "error" });
   });
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send("Internal Server Error");
+  console.error("Error details:", err);
+  res.status(500).send({ msg: "Internal Server Error", error: err.message });
 });
-
-// Connect to MongoDB Atlas and start server
-client
-  .connect()
-  .then(() => {
-    console.log("Connected to MongoDB Atlas!");
-    db = client.db(dbName);
-    app.listen(3000, () => console.log("App started on port 3000"));
-  })
-  .catch(err => {
-    console.error("Error connecting to MongoDB Atlas", err);
-    process.exit(1);
-  });
